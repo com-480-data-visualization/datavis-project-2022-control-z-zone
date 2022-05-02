71f4d786911919988ecdf6722956207b2ef0f1d8
coraliegrobel <coralie.grobel@epfl.ch>
2022-05-02 15:55:17 +0200

    add scatter plot

--- js/main.js
+++ js/main.js
@@ -139,6 +139,72 @@ class MapPlot {
 	}
 }
 
+/*For the moment we use data from serie 4 to do the scatter plot but then
+it will be the hit rate */
+
+const TEST_TEMPERATURES = [13, 18, 21, 19, 26, 25, 16];
+const DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+class ScatterPlot {
+	constructor(svg_element_id, data) {
+		this.data = data;
+		this.svg = d3.select('#' + svg_element_id);
+
+		console.log(this.svg);
+
+		this.plot_area = this.svg.append('g')
+			.attr('x', 10)
+			.attr('y', 10);
+
+		this.plot_area.append('rect')
+			.classed('plot-background', true)
+			.attr('width', 200)
+			.attr('height', 100);
+
+		const x_value_range = [d3.min(data, d => d.x), d3.max(data, d => d.x)];
+
+		const y_value_range = [0, d3.max(data, d => d.y)];
+
+		const pointX_to_svgX = d3.scaleLinear()
+			.domain(x_value_range)
+			.range([0, 200]);
+
+		const pointY_to_svgY = d3.scaleLinear()
+			.domain(y_value_range)
+			.range([100, 0]);
+
+		this.plot_area.selectAll("circle")
+			.data(data)
+			.enter()
+			.append("circle")
+				.attr("r", 1.5) // radius
+				.attr("cx", d => pointX_to_svgX(d.x)) // position, rescaled
+				.attr("cy", d => pointY_to_svgY(d.y))
+				.classed('cold', d => d.y <= 17) // color classes
+				.classed('warm', d => d.y >= 23);
+
+		// Create a label for each point
+		this.svg.append('g')
+			.selectAll('text')
+			.data(data)
+			.enter()
+				.append('text')
+				.text( d => d.name )
+				.attr('x', d => pointX_to_svgX(d.x))
+				.attr('y', 105);
+
+		// Create Y labels
+		const label_ys = Array.from(Array(6), (elem, index) => 20 * index); // 0 20 40 ... 180
+		this.svg.append('g')
+			.selectAll('text')
+			.data(label_ys)
+			.enter()
+				.append('text')
+				.text( svg_y => pointY_to_svgY.invert(svg_y).toFixed(1) )
+				.attr('x', -5)
+				.attr('y', svg_y => svg_y + 1);
+	}
+}
+
 function whenDocumentLoaded(action) {
 	if (document.readyState === "loading") {
 		document.addEventListener("DOMContentLoaded", action);
@@ -152,4 +218,10 @@ whenDocumentLoaded(() => {
 	plot_object = new MapPlot('map-plot');
 	plot_object = new MapPlot('map-plot2');
 	// plot object is global, you can inspect it in the dev-console
+	let data = TEST_TEMPERATURES.map((value, index) => {
+		return {'x': index, 'y': value, 'name': DAYS[index]};
+	});
+	
+	let plot = new ScatterPlot('plot', data);
+
 });
